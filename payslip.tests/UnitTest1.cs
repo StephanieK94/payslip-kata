860e58f968a04569855bf96392bb500a8fd045ad
Stephanie <stephanie.ktichen@serko.com>
2019-04-09 16:46:11 +1200

    Added test to get first name

--- payslip.tests/UnitTest1.cs
+++ payslip.tests/UnitTest1.cs
@@ -1,14 +1,39 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace payslip.tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GivenStringSeparateByComma()
+        {
+            string employeeFile = "David,Rudd,60050,9%,01 March – 31 March";
+
+            var commaSeparator = new CommaSeparator();
+
+            var firstName = commaSeparator.GetFirstNameFrom(employeeFile);
+
+            Assert.Equal("David", firstName);
+        }
+
+    }
+
+    internal class CommaSeparator
+    {
+        public CommaSeparator()
         {
 
+        }
+
+        internal string GetFirstNameFrom(string employeeFile)
+        {
+            var employeeDetails = employeeFile.Split(",");
+
+            var firstName = employeeDetails[0];
+
+            return firstName;
         }
     }
 }

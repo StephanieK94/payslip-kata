1064c4fa613f15187f50ff297dc05a30e4935f64
Stephanie <stephanie.ktichen@serko.com>
2019-04-12 11:29:40 +1200

    Revert

--- Payslip/PayslipCompiler.cs
+++ Payslip/PayslipCompiler.cs
@@ -2,21 +2,28 @@ using System;
 
 namespace Payslip
 {
-    public class StatementCompiler
+    public class PayslipCompiler
     {
         public string CompileStrings(EmployeeInformation employee)
         {
             EmployeeInformation employeeInfo = employee;
 
+            var fullName = GetFullNameFrom(employeeInfo.FirstName, employeeInfo.LastName);
+
             string[] employeeStrings =
             {
-                employeeInfo.FullName, employeeInfo.PayPeriod, employeeInfo.GrossIncome, employeeInfo.IncomeTax,
+                fullName, employeeInfo.PayPeriod, employeeInfo.GrossIncome, employeeInfo.IncomeTax,
                 employeeInfo.NetIncome, employeeInfo.SuperAmount
             };
 
             string employeeStatement = String.Join(",", employeeStrings);
 
             return employeeStatement;
+        }
+
+        public string GetFullNameFrom(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
         }
     }
 }

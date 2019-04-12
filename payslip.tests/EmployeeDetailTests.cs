d2628ff6a5df62a64b69be74db88ef3ba2c23595
Stephanie <stephanie.ktichen@serko.com>
2019-04-12 15:05:20 +1200

    Refactoring tests to ensure testing singular methods

--- payslip.tests/EmployeeDetailTests.cs
+++ payslip.tests/EmployeeDetailTests.cs
@@ -7,7 +7,7 @@ namespace payslip.tests
     public class EmployeeDetailTests
     {
         [Fact]
-        public void GivenEmployeeInformationGetStringStatement()
+        public void GivenEmployeeInformationAndCalculationInformationGetStringStatement()
         {
             EmployeeInformation employee = new EmployeeInformation();
             CalculationInformation calculation = new CalculationInformation();
@@ -18,18 +18,39 @@ namespace payslip.tests
             employee.PayRate = "9%";
             employee.PayPeriod = "01 March – 31 March";
 
+            calculation.GrossIncome = "5004";
+            calculation.IncomeTax = "922";
+            calculation.NetIncome = "4082";
+            calculation.SuperAmount = "450";
+
             var compiler = new PayslipCompiler();
-
-            var calculator = new Calculation();
-
-            calculation = calculator.GetCalculations(employee);
 
             var outputStatement = compiler.CompileStrings(employee, calculation);
 
             Assert.Equal("David Rudd,01 March – 31 March,5004,922,4082,450", outputStatement);
         }
 
-        
+        [Fact]
+        public void GivenEmployeeInformationGetCalculationInformation()
+        {
+            EmployeeInformation employee = new EmployeeInformation();
+            CalculationInformation calculation = new CalculationInformation();
+
+            employee.FirstName = "David";
+            employee.LastName = "Rudd";
+            employee.Salary = "60050";
+            employee.PayRate = "9%";
+            employee.PayPeriod = "01 March – 31 March";
+
+            var calculator = new Calculation();
+
+            calculation = calculator.GetCalculations(employee);
+
+            Assert.Equal("5004", calculation.GrossIncome);
+            Assert.Equal("922", calculation.IncomeTax);
+            Assert.Equal("4082", calculation.NetIncome);
+            Assert.Equal("450", calculation.SuperAmount);
+        }
 
         [Fact]
         public void GivenNameStringsReturnFullName()

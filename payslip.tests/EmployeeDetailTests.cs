704e1aa3e9d10f070b5904d3cc26ed374dcb6317
Stephanie <stephanie.ktichen@serko.com>
2019-04-12 13:58:35 +1200

    Added CalculationTests to separate from Employee information tests

--- payslip.tests/EmployeeDetailTests.cs
+++ payslip.tests/EmployeeDetailTests.cs
@@ -39,24 +39,6 @@ namespace payslip.tests
             Assert.Equal("David Rudd", fullName);
         }
 
-
-        [Fact]
-        public void GivenDecimalNumberGetsRoundedNumberToNearestInteger()
-        {
-            var pointFourNine = 10.49;
-            var pointFive = 10.5;
-            var pointFiveOne = 10.51;
-
-            var rounder = new Calculation();
-            var roundDown = rounder.GetRoundedCalculationAsInteger(pointFourNine);
-            var roundMiddleCalculation = rounder.GetRoundedCalculationAsInteger(pointFive);
-            var roundUp = rounder.GetRoundedCalculationAsInteger(pointFiveOne);
-
-            Assert.Equal(10, roundDown);
-            Assert.Equal(11, roundMiddleCalculation);
-            Assert.Equal(11, roundUp);
-        }
-
         [Fact]
         public void GivenPayRateStringGetTrimmedString()
         {

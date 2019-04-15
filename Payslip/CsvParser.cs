879d748ab74c83e683a201ff3f6494d7869cea18
Stephanie <stephanie.ktichen@serko.com>
2019-04-15 16:41:56 +1200

    Moved CsvParser into own file

---
+++ Payslip/CsvParser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Payslip
+{
+    public class CsvParser
+    {
+        public List<string> GetCsvContents(string pathName)
+        {
+            List<string> employeeStrings = new List<string>();
+
+            using (var reader = new StreamReader(pathName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    employeeStrings.Add(line);
+                }
+            }
+
+            return employeeStrings;
+        }
+    }
+}
--- payslip.tests/CSVTests.cs
+++ payslip.tests/CSVTests.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json.Linq;
+using Payslip;
 using Xunit;
 
 namespace payslip.tests
@@ -10,45 +11,18 @@ namespace payslip.tests
     public class CSVTests
     {
         [Fact]
-        public void GetCsvFile()
-        {
-            string path = Directory.GetCurrentDirectory();
-
-            string pathName = Path.GetDirectoryName(path);
-
-            Assert.Equal("C:\\Users\\StephanieK\\source\\payslip-kata\\payslip.tests\\bin\\Debug", pathName);
-        }
-
-        [Fact]
         public void ReadCsvFile()
         {
             string path = Directory.GetCurrentDirectory();
 
             var pathName = $"{path}\\Employee.csv";
+            var parser = new CsvParser2();
 
-            List<string> lines = GetCsvContents(pathName);
+            List<string> lines = parser.GetCsvContents(pathName);
 
             Assert.Equal("David,Rudd,60050,9%,01 March – 31 March", lines[0]);
             Assert.Equal("Ryan,Chen,120000,10%,01 March – 31 March", lines[1]);
 
-        }
-
-
-        private List<string> GetCsvContents(string pathName)
-        {
-            List<string> employeeStrings = new List<string>();
-
-            using (var reader = new StreamReader(pathName))
-            {
-                while (!reader.EndOfStream)
-                {
-                    string line = reader.ReadLine();
-
-                    employeeStrings.Add(line);
-                }
-            }
-
-            return employeeStrings;
         }
     }
 }

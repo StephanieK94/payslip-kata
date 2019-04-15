faf002dbb7f2766fcf0832deef3b007dc714a204
Stephanie <stephanie.ktichen@serko.com>
2019-04-16 09:42:07 +1200

    Removal of duplicate tests with same logic

--- payslip.tests/CSVTests.cs
+++ payslip.tests/CSVTests.cs
@@ -21,6 +21,10 @@ namespace payslip.tests
             var lineItems = parser.GetCsvContents(pathName);
 
             Assert.Equal("David", lineItems[0].FirstName);
+            Assert.Equal("Rudd", lineItems[0].LastName);
+            Assert.Equal(60050M, lineItems[0].Salary);
+            Assert.Equal(9, lineItems[0].PayRate);
+            Assert.Equal("01 March – 31 March", lineItems[0].PayPeriod);
         }
     }
 }
